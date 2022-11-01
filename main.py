ffe472e9523468a93b61f6279331fba527991002
boliwar <boliwar@gmail.com>
2022-11-01 14:46:30 +0200

    Passing a token to a functions

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def create_parser ():
     parser.add_argument ('url', nargs='?')
     return parser
 
-def shorten_link(url, headers):
+def shorten_link(url, bitly_access_token):
+    headers = {"Authorization": f"Bearer {bitly_access_token}"}
     shorten_res = requests.post("https://api-ssl.bitly.com/v4/shorten",
                                 json={"long_url": url},
                                 headers=headers)
@@ -18,12 +19,14 @@ def shorten_link(url, headers):
     short_url = shorten_res.json().get("link")
     return short_url
 
-def is_bitlink(url, headers):
+def is_bitlink(url, bitly_access_token):
+    headers = {"Authorization": f"Bearer {bitly_access_token}"}
     url_components = urllib.parse.urlparse(url)
     bitlink_res = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{url_components.netloc}{url_components.path}", headers=headers)
     return bitlink_res.ok
 
-def count_clicks(url, headers):
+def count_clicks(url, bitly_access_token):
+    headers = {"Authorization": f"Bearer {bitly_access_token}"}
     url_components = urllib.parse.urlparse(url)
     clicks_res = requests.get(f"https://api-ssl.bitly.com//v4/bitlinks/{url_components.netloc}{url_components.path}/clicks/summary", headers=headers)
     clicks_res.raise_for_status()
@@ -33,7 +36,6 @@ def count_clicks(url, headers):
 def main():
     load_dotenv()
     bitly_access_token = os.environ['BITLY_ACCESS_TOKEN']
-    headers = {"Authorization": f"Bearer {bitly_access_token}"}
 
     parser = create_parser()
     command_line_arguments = parser.parse_args()
@@ -41,11 +43,11 @@ def main():
     input_url = command_line_arguments.url
 
     try:
-        if not is_bitlink(input_url, headers):
-            short_url = shorten_link(input_url, headers)
+        if not is_bitlink(input_url, bitly_access_token):
+            short_url = shorten_link(input_url, bitly_access_token)
             print(f"Битлинк: {short_url}")
         else:
-            sum_clicks = count_clicks(input_url, headers)
+            sum_clicks = count_clicks(input_url, bitly_access_token)
             print(f"По вашей ссылки прошли {sum_clicks} раза")
     except requests.exceptions.HTTPError:
         print(f'Wrong URL: {input_url}')
